f62ac6467ed3cbc56e8bbd21f47f8cc6ab4fe4d3
Fabian Egartner <fabian.egartner@hotmail.com>
2024-06-10 15:47:07 +0200

    reading data from static database works

--- src/main/java/at/fhv/streamprocessing/flink/DemoWeatherDataJob.java
+++ src/main/java/at/fhv/streamprocessing/flink/DemoWeatherDataJob.java
@@ -1,41 +1,38 @@
 package at.fhv.streamprocessing.flink;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.formats.csv.CsvReaderFormat;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
-import java.io.File;
 
 public class DemoWeatherDataJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-//        DataStream<String> weatherDataStream = env
-//                .addSource(new WeatherDataMockSource())
-//                .name("weather-data");
+        DataStream<String> weatherDataStream = env
+                .addSource(new WeatherDataMockSource())
+                .name("weather-data");
 
-        CsvReaderFormat<LocationPojo> csvFormat = CsvReaderFormat.forPojo(LocationPojo.class);
-        FileSource<LocationPojo> source =
-                FileSource.forRecordStreamFormat(csvFormat,
-                        Path.fromLocalFile(new File("src/main/resources/master-location-identifier-database-202401_standard.csv"))).build();
+        CsvMapper mapper = new CsvMapper();
+        CsvReaderFormat<LocationPojo> csvFormat = CsvReaderFormat.forSchema(mapper, mapper.schemaFor(LocationPojo.class).withQuoteChar('"').withColumnSeparator(','), TypeInformation.of(LocationPojo.class));
+
+        // Use the path inside the container
+        String csvFilePath = "/opt/flink/resources/master-location-identifier-database-202401_standard.csv";
+
+        FileSource<LocationPojo> source = FileSource.forRecordStreamFormat(
+                csvFormat,
+                new Path(csvFilePath)
+        ).build();
 
         DataStream<LocationPojo> locationStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "csvFileSource");
 
         locationStream
-        .addSink(new LoggingSink())
-        .name("noaa-logging-sink");
-
-//        DataStream<NoaaRecord> noaaRecords = weatherDataStream
-//                .keyBy(a -> a)
-//                .process(new NoaaRecordParser())
-//                .name("noaa-record-parser");
-
-//        noaaRecords
-//                .addSink(new NoaaLoggingSink())
-//                .name("noaa-logging-sink");
+                .addSink(new LoggingSink())
+                .name("noaa-logging-sink");
 
         env.execute("weather-data-demo-job");
     }
--- src/main/java/at/fhv/streamprocessing/flink/LocationPojo.java
+++ src/main/java/at/fhv/streamprocessing/flink/LocationPojo.java
@@ -1,8 +1,17 @@
 package at.fhv.streamprocessing.flink;
 
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.formats.csv.CsvReaderFormat;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.apache.flink.util.function.SerializableFunction;
 
+import java.io.File;
 import java.math.BigDecimal;
+import java.util.function.Function;
 
 @JsonPropertyOrder({"country3","country2","country","region","subregion",
         "place_name","station_name","type", "stn_key", "status", "icao",
@@ -24,10 +33,35 @@ public class LocationPojo {
     public String wban;
     public String ghcn;
     public String special;
-    public BigDecimal lat;
-    public BigDecimal lng;
-    public Double elev;
+    public String lat;
+    public String lon;
+    public String elev;
     public String tz;
-}
 
+    @Override
+    public String toString() {
+        return "LocationPojo{" +
+                "country3='" + country3 + '\'' +
+                ", country2='" + country2 + '\'' +
+                ", country='" + country + '\'' +
+                ", region='" + region + '\'' +
+                ", subregion='" + subregion + '\'' +
+                ", place_name='" + place_name + '\'' +
+                ", station_name='" + station_name + '\'' +
+                ", type='" + type + '\'' +
+                ", stn_key='" + stn_key + '\'' +
+                ", status='" + status + '\'' +
+                ", icao='" + icao + '\'' +
+                ", national_id='" + national_id + '\'' +
+                ", wmo='" + wmo + '\'' +
+                ", wban='" + wban + '\'' +
+                ", ghcn='" + ghcn + '\'' +
+                ", special='" + special + '\'' +
+                ", lat='" + lat + '\'' +
+                ", lon='" + lon + '\'' +
+                ", elev='" + elev + '\'' +
+                ", tz='" + tz + '\'' +
+                '}';
+    }
+}
 
