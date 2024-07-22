8eb9647072103f66c09448d37f0e8c33ea0f9641
1r-1na <irij666@gmail.com>
2024-07-22 22:21:17 +0200

    test dashboard

--- noaa_dashboards/main.py
+++ noaa_dashboards/main.py
@@ -1,8 +1,12 @@
 import psycopg2
 import psycopg2.extensions
+import pandas as pd
+import plotly.express as px
 import select
 import threading
 import time
+from decimal import Decimal
+import datetime
 
 # Database connection parameters
 db_params = {
@@ -13,9 +17,48 @@ db_params = {
     'port': '5432'
 }
 
+def fetch_countries(connection):
+    q = "select distinct country from aggregated_data;"
+    try:
+        cursor = connection.cursor()
+        cursor.execute(q)
+        result = cursor.fetchall()
+        print(result)
+        if result:
+            return result
+        return None
+    except Exception as error:
+        print(f"Error fetching data: {error}")
+        cursor.close()
+        return None
 
-def update_plot():
-    print("HI")
+def fetch_data(connection, query):
+    try:
+        cursor = connection.cursor()
+        #cursor.mogrify(query)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if result:
+            return result
+        return None
+    except Exception as error:
+        print(f"Error fetching data: {error}")
+        cursor.close()
+        return None
+
+def update_plot(connection):
+    countries = fetch_countries(connection)
+
+    if countries:
+        for country in countries:
+            # because its a tuple
+            c = country[0]
+            q = f"select aggregation_type, start_ts, value from aggregated_data where country = '{c}' and measurement_type = 'TEMPERATURE' order by start_ts;"
+            data = fetch_data(connection, q)
+            if data:
+                df = pd.DataFrame(data, columns=['AggType', 'Day', 'Value'])
+                fig = px.line(df, x="Day", y="Value", color='AggType', title=f"Temperature per day in {c}")
+                fig.show()
 
 
 def listen_notifications(callback):
@@ -32,7 +75,7 @@ def listen_notifications(callback):
         while conn.notifies:
             notify = conn.notifies.pop(0)
             print("Got NOTIFY:", notify.payload)
-            callback()
+            callback(conn)
             # sleep to not update too continuously
             time.sleep(10)
 
