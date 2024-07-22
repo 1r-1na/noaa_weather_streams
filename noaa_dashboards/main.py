d20aef2b66ded6800e28d03c9ec8aaf5b831a7ba
1r-1na <irij666@gmail.com>
2024-07-22 21:38:32 +0200

    dashboards

---
+++ noaa_dashboards/main.py
@@ -0,0 +1,43 @@
+import psycopg2
+import psycopg2.extensions
+import select
+import threading
+import time
+
+# Database connection parameters
+db_params = {
+    'dbname': 'default',
+    'user': 'dev_user',
+    'password': 'dev_pw',
+    'host': 'localhost',
+    'port': '54321'
+}
+
+
+def update_plot():
+    print("HI")
+
+
+def listen_notifications(callback):
+    conn = psycopg2.connect(**db_params)
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = conn.cursor()
+    cursor.execute()
+    cursor.execute("LISTEN custom_channel;")
+    print("Listening for notifications on channel 'custom_channel'...")
+
+    while True:
+        if select.select([conn], [], [], 5) == ([], [], []):
+            continue
+        conn.poll()
+        while conn.notifies:
+            notify = conn.notifies.pop(0)
+            print("Got NOTIFY:", notify.payload)
+            callback()
+            # sleep to not update too continuously
+            time.sleep(10)
+
+
+if __name__ == "__main__":
+    listener_thread = threading.Thread(target=listen_notifications, args=(update_plot,))
+    listener_thread.start()
